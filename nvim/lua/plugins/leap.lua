540aa9c338eccd6c9f5bfa6aaa0bc27a5b34ce00
fkkehlet <fkehlet@protonmail.com>
2026-03-19 12:43:09 +0100

    update

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -5,7 +5,7 @@ return {
         require 'leap'
 
         vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-        vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+        vim.keymap.set({'n', 'o'}, 'S',  '<Plug>(leap-backward)')
         vim.keymap.set('n',             'gs', '<Plug>(leap-from-window)')
     end
 }
